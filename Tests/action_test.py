0c42af620bf31221ee61dbfc062ca49b6a276ca4
Matt Kershaw <mattkershaw@capuk.org>
2025-02-03 15:55:53 +0000

    Test playing card

---
+++ Tests/action_test.py
@@ -0,0 +1,41 @@
+from DataTransfer.GameData import GameData
+from Server.ServerApp import ServerApp
+
+
+def test_play_card():
+    game_json = {
+        'active_player_index': 0,
+        'graveyard': {'cards': []},
+        'players': [
+            {
+                'team': 'Team 1',
+                'hand': {'cards': [{
+                    'card_id': '1234',
+                    'value': 'A',
+                }]},
+                'deck': {'cards': []},
+                'known_cards': [],
+                'actions': 3,
+            },
+            {
+                'team': 'Team 2',
+                'hand': {'cards': []},
+                'deck': {'cards': []},
+                'known_cards': [],
+                'actions': 0,
+            },
+        ],
+        'board': {'lanes': [
+            {'sides': [{'cards': []}, {'cards': []}]},
+            {'sides': [{'cards': []}, {'cards': []}]},
+            {'sides': [{'cards': []}, {'cards': []}]},
+        ]},
+        'winner': None,
+    }
+    server_app = ServerApp('test', GameData.from_json(game_json).make_server())
+    server_app.resolve_action({'event': 'play', 'data': {'side': 1, 'card': '1234'}})
+
+    assert len(server_app.game.players[0].hand.cards) == 0
+    played_card = server_app.game.board.lanes[0].sides[1].cards[0]
+    assert played_card.card_id == '1234'
+    assert played_card.value == 'A'
