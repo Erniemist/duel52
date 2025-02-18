c0ab427dd93652973e0027332c14a79e132dfa23
Thomas Kershaw <tom@kershaw.org>
2025-02-18 11:37:51 +0000

    Add failing test for HP increase on Jacks

---
+++ Tests/CardTypes/test_jack.py
@@ -0,0 +1,24 @@
+from DataTransfer.CardData import CardData
+from GameFactory import GameFactory
+
+
+def test_jack():
+    factory = GameFactory()
+    factory.with_hand('A', [
+        CardData('J', 'J'),
+    ])
+    factory.with_hand('B', [
+        CardData('A', 'A'),
+        CardData('X_2', 'X'),
+    ])
+    game = factory.make_server()
+
+    game.play('J', '111')
+    game.flip('J')
+
+    game.new_turn()
+    game.play('A', '222')
+    game.flip('A')
+    game.attack('A', ['J'])
+    game.attack('A', ['J'])
+    assert len(game.find_side('111').cards) == 1
