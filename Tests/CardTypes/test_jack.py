7b0cede3d61f4e2a5aa44ef3c1fc3c14bc2295af
Matt Kershaw <mattkershaw@capuk.org>
2025-02-27 12:32:03 +0000

    Test can attack Jack with other Jack in lane

--- Tests/CardTypes/test_jack.py
+++ Tests/CardTypes/test_jack.py
@@ -25,10 +25,12 @@ def test_jack_hp():
     game.attack('A', ['J'])
     assert len(game.find_side('111').cards) == 1
 
+
 def test_jack_taunt():
     factory = GameFactory()
     factory.with_hand('A', [
-        CardData('J', 'J'),
+        CardData('J_1', 'J'),
+        CardData('J_2', 'J'),
         CardData('X_1', 'X'),
     ])
     factory.with_hand('B', [
@@ -37,12 +39,17 @@ def test_jack_taunt():
     ])
     game = factory.make_server()
 
-    game.active_player().actions = 3
-    game.play('J', '111')
-    game.flip('J')
+    game.active_player().actions = 5
+    game.play('J_1', '111')
+    game.flip('J_1')
+    game.play('J_2', '111')
+    game.flip('J_2')
     game.play('X_1', '111')
 
     game.play('A', '222')
     game.flip('A')
     with pytest.raises(Exception, match='Tried to attack a non-Jack minion'):
         game.attack('A', ['X_1'])
+    game.attack('A', ['J_1'])
+    assert game.find_card('J_1').minion.hp == 2
+    assert game.find_card('J_2').minion.hp == 3
